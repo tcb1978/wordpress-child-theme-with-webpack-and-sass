f4013e4c378a0ef4e1e576b1ad0bb130408505f8
Matthew Eldredge <mattheweldredge@gmail.com>
2018-08-05 15:30:07 -0700

    wordpress child theme with webpack and sass

---
+++ functions.php
@@ -0,0 +1,11 @@
+// register webpack stylesheet and js with theme
+wp_enqueue_style( 'site_main_css', get_template_directory_uri() . '/css/build/main.min.css' );
+wp_enqueue_script( 'site_main_js', get_template_directory_uri() . '/js/build/app.min.css' , null , null , true );
+// include the css file
+$cssFilePath = glob( get_template_directory() . '/css/build/main.min.*' );
+$cssFileURI = get_template_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
+wp_enqueue_style( 'site_main_css', $cssFileURI );
+// include the javascript file
+$jsFilePath = glob( get_template_directory() . '/js/build/app.min.*.js' );
+$jsFileURI = get_template_directory_uri() . '/js/build/' . basename($jsFilePath[0]);
+wp_enqueue_script( 'site_main_js', $jsFileURI , null , null , true );
